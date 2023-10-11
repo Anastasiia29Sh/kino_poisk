d61c70925a361b6e999f12285713f7dbcdc04824
Anastasiia29Sh <nastya.sharcko@yandex.ru>
2023-10-11 13:39:44 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/kino_poisk/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/kino_poisk/'
+    : '/',
   transpileDependencies: true
 })
